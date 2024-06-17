391dbfed08c503edb42a8ca03cc18137b8540474
professionalsquirrel <71053041+professionalsquirrel@users.noreply.github.com>
2024-06-17 16:25:15 +0100

    Generates password + Add copy feature

---
+++ script.js
@@ -0,0 +1,30 @@
+const passwordBox = document.getElementById("password");
+const display = document.querySelector("display");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+const generateButton = document.getElementById("btn");
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    passwordBox.value = password;
+};
+
+generateButton.addEventListener("click", createPassword);
+
+
